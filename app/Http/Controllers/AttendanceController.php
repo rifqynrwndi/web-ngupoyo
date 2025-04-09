df3911ba57853b6a032357c795f74ebb00ebd0c8
Rifqy Nirwandi <nirwandyrifqy@students.unnes.ac.id>
2025-04-09 13:05:16 +0800

    init project

---
+++ app/Http/Controllers/AttendanceController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Attendance;
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+    //index
+    public function index(Request $request)
+    {
+        $attendances = Attendance::with('user')
+            ->when($request->input('name'), function ($query, $name) {
+                $query->whereHas('user', function ($query) use ($name) {
+                    $query->where('name', 'like', '%' . $name . '%');
+                });
+            })->orderBy('id', 'desc')->paginate(10);
+        return view('pages.absensi.index', compact('attendances'));
+    }
+
+    //edit
+    public function edit($id)
+    {
+        $attendance = Attendance::findOrFail($id);
+        return view('pages.absensi.edit', compact('attendance'));
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'user',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        // data dummy for company
+        \App\Models\Company::create([
+            'name' => 'PT. FIC16',
+            'email' => 'fic16@codewithbahri.com',
+            'address' => 'Jl. Raya Kedung Turi No. 20, Sleman, DIY',
+            'latitude' => '-7.747033',
+            'longitude' => '110.355398',
+            'radius_km' => '0.5',
+            'time_in' => '08:00',
+            'time_out' => '17:00',
+        ]);
+
+        $this->call([
+            AttendanceSeeder::class,
+            PermissionSeeder::class,
+        ]);
+    }
+}
